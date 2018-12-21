80815fa28f508416de75b6fa66384320e242ddb0
Marcos Monjon Mas <sysmarc13@hotmail.com>
2018-12-21 03:26:43 +0100

    Iniciación a Paquetes, en este caso uno de cáculos básicos

---
+++ POO/paquetecalculos-1.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+
+
+	name="paquetecalculos",
+	version="1.0",
+	description="Paquete de calculos",
+	author="Marcos",
+	autor_email="sysmarc13@hotmail.com",
+	packages=["calculos"]
+
+	)
