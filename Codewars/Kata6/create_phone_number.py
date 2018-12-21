ce4ec259eda25900a44d03c10d7529a71564b134
marc monjon <sysmarc13@hotmail.com>
2018-12-21 16:58:22 +0100

    Ejercicio Codewars crear un numero de telefono

---
+++ Codewars/Kata6/create_phone_number.py
@@ -0,0 +1,10 @@
+def create_phone_number(number):
+    phone = ''.join(str(digit) for digit in number)
+    return "(" + phone[0:3] + ")" + " " + phone[3:6] + "-" + phone[6:]
+
+
+assert create_phone_number([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) == "(123) 456-7890"
+assert create_phone_number([1, 1, 1, 1, 1, 1, 1, 1, 1, 1]) == "(111) 111-1111"
+assert create_phone_number([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) == "(123) 456-7890"
+assert create_phone_number([0, 2, 3, 0, 5, 6, 0, 8, 9, 0]) == "(023) 056-0890"
+assert create_phone_number([0, 0, 0, 0, 0, 0, 0, 0, 0, 0]) == "(000) 000-0000"
