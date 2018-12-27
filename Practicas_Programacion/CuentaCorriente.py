a8020c5b287edb2d1818b8c29651e1ab19f81780
marc monjon <sysmarc13@hotmail.com>
2018-12-27 20:38:05 +0100

    Modificacion

--- Practicas_Programacion/CuentaCorriente.py
+++ Practicas_Programacion/CuentaCorriente.py
@@ -15,12 +15,16 @@ class CuentaCorriente():
 
 		self.__saldo-=int(dineroretirado)
 
+		return self.__saldo
+
 
 	def setIngresarDinero(self,dineroingresado):
 
 		self.dineroingresado=int(dineroingresado)
 
 		self.__saldo+=int(dineroingresado)
+
+		return self.__saldo
 
 
 	def getNombre(self):
@@ -47,8 +51,8 @@ class CuentaCorriente():
 
 	def consultarCuenta(self):
 		
-		print("Nombre: " , self.getNombre(),"\nApellidos: ", self.getApellidos(),
-			"\nDireccion: ", self.getDireccion(), "\nTelefono: ", self.getTelefono(),"\nSaldo: ", self.getSaldo())
+		print( "Nombre: " , self.getNombre(), "\nApellidos: ", self.getApellidos(),"\nDireccion: ", self.getDireccion(), "\nTelefono: ", self.getTelefono(),"\nSaldo: ", self.getSaldo())
+
 
 		
 
@@ -56,20 +60,23 @@ class CuentaCorriente():
 		
 
 		if self.__saldo >0:
-			return True
-		else:
 			return False
+		else:
+			return True
 
 miCuentaCorriente=CuentaCorriente("Marcos","Monjon M","Cfemenias","61722222","3724712s")
 
+if __name__ == "__main__":
+
+	assert miCuentaCorriente.setIngresarDinero(100) ==  1100.0
+	assert miCuentaCorriente.setRetirarDinero(400) == 700.0
+	assert miCuentaCorriente.saldoNegativo() == False
+	assert miCuentaCorriente.setRetirarDinero(800) == -100.0
+	assert miCuentaCorriente.saldoNegativo() == True
+	assert miCuentaCorriente.getNombre() == "Marcos"
+	assert miCuentaCorriente.getSaldo() == -100.0
 
 
 
-miCuentaCorriente.setIngresarDinero(100)
-
-
-miCuentaCorriente.setRetirarDinero(400)
-
-miCuentaCorriente.consultarCuenta()
 
 
