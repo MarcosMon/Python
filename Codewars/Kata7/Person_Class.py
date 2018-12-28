2d0d461c224e474f178520560463e3c8fa5665c3
marc monjon <sysmarc13@hotmail.com>
2018-12-28 14:27:11 +0100

    Kata 7 POO

---
+++ Codewars/Kata7/Person_Class.py
@@ -0,0 +1,16 @@
+class Person():
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+        self.full_name = first_name + " " + last_name
+
+
+matz = Person('Yukihiro', 'Matsumoto', 47)
+assert matz.full_name == 'Yukihiro Matsumoto'
+assert matz.age == 47
+
+joe = Person('Joe', 'Smith', 30)
+assert joe.full_name == 'Joe Smith'
+assert joe.age == 30
