51c19ebcd5fcf2ccee5c03870f2933cc846545ca
marc monjon <sysmarc13@hotmail.com>
2019-01-09 19:15:18 +0100

    Casos test

--- Practicas_Programacion/GildedRoseClass/backstage.py
+++ Practicas_Programacion/GildedRoseClass/backstage.py
@@ -2,17 +2,17 @@ from regularItem import RegularItem
 class Backstage(RegularItem):
 
 	def update_quality(self):
-		if self.sell_in <=10:
-			self.setQuality(2)
-		elif self.sell_in <=5:
-			self.setQuality(3)
-		else:
-			self.setQuality(2)
-		self.setSell_in()
 
 		
+		if self.sell_in <=10 and self.sell_in >5:
+			self.setQuality(2)
 
+		elif self.sell_in <=5 and self.sell_in >0:
+			self.setQuality(3)
 
+		elif self.sell_in  <= 0:
+			self.setQuality(0)
+		self.setSell_in()
 
 
 
@@ -20,8 +20,23 @@ class Backstage(RegularItem):
 if __name__ == "__main__":
 
 
-	miBackstage=Backstage("Backstage",4,8)
+	miBackstage=Backstage("Backstage",6,8)
 
 	miBackstage.update_quality()
 
 	assert miBackstage.getQuality()== 10
+
+
+	miBackstage1=Backstage("Backstage",4,40)
+
+	miBackstage1.update_quality()
+
+	assert miBackstage1.getQuality()== 43
+
+	miBackstage2=Backstage("Backstage",-2,40)
+
+	miBackstage2.update_quality()
+
+	assert miBackstage2.getQuality()== 40
+
+
