7adf8ddd77b573660c13ffdb7449fa0c66fb57e5
marc monjon <sysmarc13@hotmail.com>
2019-01-12 01:20:41 +0100

    Ejercicio TarjetaPrepago POO

--- Practicas_Programacion/tarjetaprepago.py
+++ Practicas_Programacion/tarjetaprepago.py
@@ -6,38 +6,67 @@ class TarjetaPrepago():
         self.numeroTelefono = str(numeroTelefono)
         self.saldo = float(saldo)
         self.nif = str(nif)
-        self.consumo = 0
+        self.consumoLlamadas = 0
+        self.consumoMensajes = 0
+        self.tiempoHablado = 0
 
-    def getSaldoActual(self):
-        return self.saldo
+    def getSaldo(self):
+        return round(self.saldo, 2)
 
     def getNif(self):
         return self.nif
 
-    def getConsultarTelefono(self):
+    def getNumeroTelefono(self):
         return self.numeroTelefono
 
     def getConsultarTarjeta(self):
-        print("Tú saldo actual es de %s€, tu DNI es %s tu número de telefono es %s y has consumido un total de %s" % (self.getSaldoActual(),self.getNif(), self.getConsultarTelefono(),self.consumo))
+        print("Tú saldo actual es de %s€, tu DNI es %s tu número de telefono es %s y has consumido un total de %s€ en llamadas y %s€ en mensajes. (Total hablado %s)" % (
+            self.getSaldo(), self.getNif(), self.getNumeroTelefono(), self.getConsumoLlamadas(), self.getConsumoMensajes(), self.getHora()))
+
+    def setNif(self):
+        self.nif = nif
+
+    def setHora(self, tiempoHablado):
+        if tiempoHablado < 60:
+            self.tiempoHablado = str(tiempoHablado) + " Segundos"
+        elif tiempoHablado < 3600:
+            self.tiempoHablado = str(round(tiempoHablado/60, 1)) + " Minutos"
+        else:
+            self.tiempoHablado = str(round(tiempoHablado/600, 2)) + " Horas"
+
+    def getHora(self):
+        return self.tiempoHablado
+
+    def setConsumoLLamadas(self, gastos):
+        self.consumoLlamadas += gastos
+
+    def getConsumoLlamadas(self):
+        return self.consumoLlamadas
+
+    def setConsumoMensajes(self, gastos):
+        self.consumoMensajes += gastos
+
+    def getConsumoMensajes(self):
+        return self.consumoMensajes
+
+    def setSaldo(self, gastos):
+        self.saldo -= gastos
+
+    def setNumeroTelefono(self):
+        self.numeroTelefono = numeroTelefono
 
     def setrealizarLlamada(self, segundos_hablados):
-        self.saldo -= (segundos_hablados+15)/100
-
-        if segundos_hablados <60:
-        	self.consumo= str(segundos_hablados) + " Segundos"
-        elif segundos_hablados <3600:
-        	self.consumo = str(round(segundos_hablados/60,1)) + " Minutos"
-        else:
-        	self.consumo= str(round(segundos_hablados/600,2)) + " Horas"
-
+        self.setSaldo((segundos_hablados+15)/100)
+        self.setConsumoLLamadas((segundos_hablados+15)/100)
+        self.setHora(segundos_hablados)
 
     def setingresarSaldo(self, añadir_saldo):
-        self.saldo += añadir_saldo
+        self.setSaldo(-añadir_saldo)
 
     def setenviarMensaje(self, mensajes):
 
-        self.saldo -= mensajes*9/100
-
+        self.setSaldo(mensajes*9/100)
+        self.setConsumoMensajes(mensajes*9/100)
 
     def __repr__(self):
         return getConsultarTarjeta()
@@ -45,11 +74,12 @@ class TarjetaPrepago():
 
 if __name__ == '__main__':
 
-    tarjeta = TarjetaPrepago("61481248", "431325462S", 20)
-    tarjeta.setenviarMensaje(5)
-    assert tarjeta.getSaldoActual() == 19.55
+    tarjeta = TarjetaPrepago("61481248", "431325462P", 20)
+    tarjeta.setenviarMensaje(72)
+    assert tarjeta.getSaldo() == 13.52
     tarjeta.setingresarSaldo(40)
-    assert tarjeta.getSaldoActual() == 59.55
+    assert tarjeta.getSaldo() == 53.52
     tarjeta.setrealizarLlamada(3700)
-    assert tarjeta.getSaldoActual() == 22.40
+    assert tarjeta.getSaldo() == 16.37
+    assert tarjeta.getConsumoMensajes() == 6.48
     tarjeta.getConsultarTarjeta()
